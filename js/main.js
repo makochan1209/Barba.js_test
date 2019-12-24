ee8562d9bc1d9b85cbdfe50ce6ab0053e918ad21
Makoto Yamashita <makochan12.9@gmail.com>
2019-12-24 14:03:17 +0900

    単純なやつ(駒場祭)

--- js/main.js
+++ js/main.js
@@ -3,37 +3,18 @@ Barba.Pjax.init();
 
 /* アニメーションの関数を記述
 --------------------------------------------------*/
-var FadeTransition = Barba.BaseTransition.extend({
-    start: function() {
+var HideShowTransition = Barba.BaseTransition.extend({
+  start: function() {
+    this.newContainerLoading.then(this.finish.bind(this));
+  },
+
+  finish: function() {
+    document.body.scrollTop = 0;
+    this.done();
+  }
+});
   
-      Promise
-        .all([this.newContainerLoading, this.fadeOut()])
-        .then(this.fadeIn.bind(this));
-    },
-  
-    fadeOut: function() {
-      return $(this.oldContainer).animate({ opacity: 0 }).promise();
-    },
-  
-    fadeIn: function() {
-  
-      var _this = this;
-      var $el = $(this.newContainer);
-  
-      $(this.oldContainer).hide();
-  
-      $el.css({
-        visibility : 'visible',
-        opacity : 0
-      });
-  
-      $el.animate({ opacity: 1 }, 400, function() {
-        _this.done();
-      });
-    }
-  });
-  
-  Barba.Pjax.getTransition = function() {
+  Barba.Pjax.HideShowTransition = function() {
     return FadeTransition;
   };
   
